a18e7ed07f0b9d0178a0401ca83b189d47f56b4f
Abraham Anyang <anyangmaker90@gmail.com>
2022-07-14 22:26:44 +0300

    lists and keys well understood

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -8,7 +8,8 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
-
+  
+/*
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
@@ -22,5 +23,50 @@ function ColorList() {
     </div>
   );
 }
+*/
+
+//const colorElements = colors.map((color) => {
+ // return <li style={{ color: color }}>{color}</li>;
+//});
+const colorElements = colors.map((color) => {
+  return (
+    <li key={color} style={{ color: color }}>
+      {color}
+    </li>
+  );
+});
+return (
+  <div>
+    <h1>Top 5 CSS Colors</h1>
+    <ol>
+      {/* display the array of <li> elements here! */}
+      {colorElements}
+    </ol>
+  </div>
+);
+}
+
+/*
+// ColorItem component
+function ColorItem(props) {
+  return <li style={{ color: props.color }}>{props.color}</li>;
+}
+
+// ColorList component
+function ColorList() {
+  const colors = [
+    "firebrick",
+    "rebeccapurple",
+    "salmon",
+    "darkslategray",
+    "hotpink",
+  ];
+
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color={color} />;
+  });
+  // etc
+}
+*/ 
 
 export default ColorList;
